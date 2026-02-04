544f8b84e86ce77038233c6bb232a237b7fa782d
secupunk <secupunk@protonmail.com>
2026-02-04 09:05:23 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,61 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async'; 
 import { ModalProvider } from './context/ModalContext';
 import LandingPage from './components/LandingPage';
 import RoutesPage from './components/pages/RoutesPage';
 import RouteDetailPage from './components/pages/RouteDetailPage';
-import ComparePage from './components/pages/ComparePage'; 
+import ComparePage from './components/pages/ComparePage';
+import CompareDirectory from './components/pages/CompareDirectory'; // Importé ici
 import FreeLandingPage from './components/pages/FreeLandingPage';
 import PrivacyPolicy from './components/pages/PrivacyPolicy';
 import TermsOfService from './components/pages/TermsOfService';
 import RefundPolicy from './components/pages/RefundPolicy';
 import Navbar from './components/Navbar';
 import ValentineCountdown from './components/ValentineCountdown';
-import useGoogleAnalytics from './hooks/useGoogleAnalytics';
-import { trackPageView as trackGAPageView } from './utils/analytics';
-import { trackPageView as trackPixelPageView } from './utils/pixelTracking';
 import { Toaster } from './components/ui/toaster';
 
 const AnalyticsWrapper = ({ children }) => {
   const location = useLocation();
   useEffect(() => {
-    trackGAPageView(location.pathname + location.search);
-    trackPixelPageView();
-    window.scrollTo(0, 0); 
+    window.scrollTo(0, 0);
   }, [location]);
   return children;
 };
 
 function App() {
-  useGoogleAnalytics();
-
   return (
-    <Router>
-      <ModalProvider>
-        <AnalyticsWrapper>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={
-              <>
-                <div className="pt-24 bg-[#0A0E27]">
-                  <ValentineCountdown />
-                </div>
-                <LandingPage />
-              </>
-            } />
-            <Route path="/routes" element={<RoutesPage />} />
-            <Route path="/routes/:slug" element={<RouteDetailPage />} />
-            
-            {/* ROUTE SIMPLIFIÉE POUR ÉVITER L'ÉCRAN BLANC */}
-            <Route path="/compare/:slug1/:slug2" element={<ComparePage />} />
-            
-            <Route path="/free" element={<FreeLandingPage />} />
-            <Route path="/privacy" element={<PrivacyPolicy />} />
-            <Route path="/terms" element={<TermsOfService />} />
-            <Route path="/refund-policy" element={<RefundPolicy />} />
-          </Routes>
-        </AnalyticsWrapper>
-        <Toaster />
-      </ModalProvider>
-    </Router>
+    <HelmetProvider>
+      <Router>
+        <ModalProvider>
+          <AnalyticsWrapper>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={
+                <>
+                  <div className="pt-24 bg-[#0A0E27]">
+                    <ValentineCountdown />
+                  </div>
+                  <LandingPage />
+                </>
+              } />
+              <Route path="/routes" element={<RoutesPage />} />
+              <Route path="/routes/:slug" element={<RouteDetailPage />} />
+              
+              {/* NOUVELLES ROUTES SEO */}
+              <Route path="/compare" element={<CompareDirectory />} />
+              <Route path="/compare/:slug1-vs-:slug2" element={<ComparePage />} />
+              
+              <Route path="/free" element={<FreeLandingPage />} />
+              <Route path="/privacy" element={<PrivacyPolicy />} />
+              <Route path="/terms" element={<TermsOfService />} />
+              <Route path="/refund-policy" element={<RefundPolicy />} />
+            </Routes>
+          </AnalyticsWrapper>
+          <Toaster />
+        </ModalProvider>
+      </Router>
+    </HelmetProvider>
   );
 }
 
